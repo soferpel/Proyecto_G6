9deeed638b14b8eceff4a29d124c0e2ee19d3031
nataliaaa12 <natalia.g@opendeusto.es>
2024-12-04 16:51:01 +0100

    Conexion bd

--- Proyecto_G6/src/db/BaseDatosConfiguracion.java
+++ Proyecto_G6/src/db/BaseDatosConfiguracion.java
@@ -1,5 +1,43 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+
 public class BaseDatosConfiguracion {
 
+	protected static Logger logger = Logger.getLogger(BaseDatosConfiguracion.class.getName());
+
+	
+	/**
+	 * Método que realiza la conexión con la base de datos
+	 * @param nombreBD : Nombre de la base de datos a la que nos vamos a conectar
+	 * @return Devuelve la conexión a la base de datos
+	 */
+	public static Connection initBD(String nombreBD) {
+		Connection con = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:"+nombreBD);
+					
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException ex) {
+			logger.warning(String.format("Error conectando con la BBDD: %s", ex.getMessage()));
+		}
+		
+		return con;
+	}
+	
+	public static void closeBD(Connection con) {
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException ex) {
+				logger.warning(String.format("Error cerrando conexión con la BBDD: %s", ex.getMessage()));
+			}
+		}
+	}
 }
